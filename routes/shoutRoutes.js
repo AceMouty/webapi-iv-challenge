b1cf3d6a1e9b374e7f4ca48e59ab6e6b610852c0
AceMouty <kyleguerrero85@gmail.com>
2019-10-03 21:02:04 -0500

    Created delte functionality
    upon delete req check to see if there are shouts if not return to the user there are no shouts
    if the array is populated find the item and remove it
    return a message the item has been removed, return the updated array, and return the item removed

--- routes/shoutRoutes.js
+++ routes/shoutRoutes.js
@@ -4,13 +4,15 @@ const router = express.Router();
 // Fake DB to be used in the api
 const shouts = [];
 
+// Read
 router.get("/", (req, res) => {
     res.status(200).json({data: shouts})
 })
 
+// Create
 router.post("/", (req, res) => {
     // check if shouts is empty or not
-    if(shouts.length < 1){
+    if(shouts.length === 0){
         shouts.push(req.body)
         // Add data to the shouts array
         res.status(201).json({message: "Shout created successfully"})
@@ -19,16 +21,53 @@ router.post("/", (req, res) => {
         // does do not allow it to be entered
         shouts.forEach(shout => { 
             if(shout.id === req.body.id){
-                res.status(404).json({message: "shout with this ID already exists"})
+                return res.status(404).json({message: "shout with this ID already exists"})
             } else {
                 shouts.push(req.body)
                 // Add data to the shouts array
-                res.status(201).json({message: "Shout created successfully"})
+                return res.status(201).json({message: "Shout created successfully"})
             }
         })
     }
 })
 
+// Update
+router.put("/", (req, res) => {
+    if(shouts.length === 0){
+        res.status(200).json({message: "There are currently no shouts"})
+    } else {
+        shouts.forEach(shout => {
+            if (shout.id === req.body.id){
+                shout.title = req.body.title
+                res.status(200).json({message: "shout has been updated", data: shout})
+            } else {
+                res.status(404).json({message: "This shout does not exist"})
+            }
+        })
+    }
+})
+
+// Delete
+router.delete('/', (req, res) => {
+    if(shouts.length === 0){
+        res.status(200).json({message: "There are currently no shouts"})
+    } else {
+        shouts.forEach((shout, i) => {
+            if(shout.id === req.body.id) {
+                const removedShout = shouts.splice(i, i+1)
+                res
+                    .status(200)
+                    .json({
+                        mesage: "Shout was removed",
+                        removedShout: removedShout,
+                        shouts: shouts
+                    })
+            } else {
+                res.status(200).json({message: "A shout with that ID does not exist"})
+            }
+        })
+    }
+})
 
 // Export the routes for endpoints with /api/shouts
 module.exports = router;
