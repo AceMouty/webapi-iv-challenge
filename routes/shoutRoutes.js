9d514d24a5f114d7579d0de9a15f738b28ca8df9
AceMouty <kyleguerrero85@gmail.com>
2019-10-03 20:20:56 -0500

    api has create functionality
    api supports post req.
    checks to see if the shouts is empty if it is insert the imcoming shout
    if the arry isnt empty check the array of shouts to see if the id exists, if it does reject the request
    if the id for the imcoming shout doesnt exist then insert the incoming shout

--- routes/shoutRoutes.js
+++ routes/shoutRoutes.js
@@ -8,6 +8,27 @@ router.get("/", (req, res) => {
     res.status(200).json({data: shouts})
 })
 
+router.post("/", (req, res) => {
+    // check if shouts is empty or not
+    if(shouts.length < 1){
+        shouts.push(req.body)
+        // Add data to the shouts array
+        res.status(201).json({message: "Shout created successfully"})
+    } else {
+        // Check if an id for a shout already exits, if it
+        // does do not allow it to be entered
+        shouts.forEach(shout => { 
+            if(shout.id === req.body.id){
+                res.status(404).json({message: "shout with this ID already exists"})
+            } else {
+                shouts.push(req.body)
+                // Add data to the shouts array
+                res.status(201).json({message: "Shout created successfully"})
+            }
+        })
+    }
+})
+
 
 // Export the routes for endpoints with /api/shouts
 module.exports = router;
